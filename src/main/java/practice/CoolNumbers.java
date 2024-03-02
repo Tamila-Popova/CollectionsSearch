79de3367de95d8b0aaa465b41e78784145b57304
tamila_popova <popova.t.t@yandex.ru>
2024-03-02 20:07:21 +0300

    Task CoolNumbers done

--- src/main/java/practice/CoolNumbers.java
+++ src/main/java/practice/CoolNumbers.java
@@ -1,30 +1,69 @@
 package practice;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
+    public static ArrayList<String> list = new ArrayList<>();
 
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        String[] letters = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
+        for (int i1 = 0; i1 < letters.length; i1++) {
+            for (int i2 = 0; i2 < letters.length; i2++) {
+                for (int i3 = 0; i3 < letters.length; i3++) {
+                    for (int j = 0; j < 10; j++) {
+                        for (int j1 = 0; j1 < 199; j1++) {
+                            String letter1 = letters[i1];
+                            String letter2 = letters[i2];
+                            String letter3 = letters[i3];
+                            String region = String.valueOf(j1);
+                            if (j1 < 10) {
+                                region = "0" + region;
+                            }
+                            String number = String.format("%s%d%d%d%s%s%s", letter1, j, j, j, letter2, letter3, region);
+                            list.add(number);
+                            System.out.println(number);
+                        }
+                    }
+                }
+            }
+        }
+        return list;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+        long before = System.nanoTime();
+        boolean num = list.contains(number);
+        long after = System.nanoTime();
+        long time = after - before;
+        System.out.println("номер " + (num ? "" : "не") + " найден, поиск занял: " + time + "нс");
+        return num;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        long before = System.nanoTime();
+        int index = Collections.binarySearch(sortedList, number);
+        boolean num = index >= 0;
+        long after = System.nanoTime();
+        long time = after - before;
+        System.out.println("номер " + (num ? "" : "не") + " найден, поиск занял: " + time + "нс");
+        return num;
     }
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        long before = System.nanoTime();
+        boolean num = hashSet.contains(number);
+        long after = System.nanoTime();
+        long time = after - before;
+        System.out.println("номер " + (num ? "" : "не") + " найден, поиск занял: " + time + "нс");
+        return num;
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        long before = System.nanoTime();
+        boolean num = treeSet.contains(number);
+        long after = System.nanoTime();
+        long time = after - before;
+        System.out.println("номер " + (num ? "" : "не") + " найден, поиск занял: " + time + "нс");
+        return num;
     }
-
 }
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,14 +1,20 @@
 package practice;
 
+import java.util.*;
+
 public class Main {
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
-
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        List<String> coolNumbers = CoolNumbers.generateCoolNumbers();
+        List<String> sortedCoolNumbers = new ArrayList<>(coolNumbers);
+        Collections.sort(sortedCoolNumbers);
+        HashSet<String> listHashSet = new HashSet<>(coolNumbers);
+        TreeSet<String> listTreeSet = new TreeSet<>(coolNumbers);
+        System.out.println("Длина списка: " + CoolNumbers.list.size());
+        String searchNumber = sc.nextLine();
+        CoolNumbers.bruteForceSearchInList(coolNumbers, searchNumber);
+        CoolNumbers.binarySearchInList(coolNumbers, searchNumber);
+        CoolNumbers.searchInHashSet(listHashSet, searchNumber);
+        CoolNumbers.searchInTreeSet(listTreeSet, searchNumber);
     }
 }
